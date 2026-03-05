9a0760390d25c0a725057e6c96a1cdbb1b3caec3
Henrique A. R. <henrique4r.it@gmail.com>
2026-03-04 21:43:11 -0300

    study: implementa a estilização de cards

--- primeiroApp/App.tsx
+++ primeiroApp/App.tsx
@@ -5,10 +5,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>🎉Bem-vindos à aula!🎉</Text>
-      <Text>📝 Vamos aprender React Native</Text>
-      <Text>🚀 Inline styles são fáceis de entender!</Text>
-      <Text>🌈🎨 Alterar cores e tamanhos é divertido!</Text>
-      <Text>✨ Desafio: tente recriar este layout!</Text>
+      <Text style={[styles.card1, styles.pattern]}>📝 Vamos aprender React Native</Text>
+      <Text style={[styles.card2, styles.pattern]}>🚀 Inline styles são fáceis de entender!</Text>
+      <Text style={[styles.card3, styles.pattern]}>🌈🎨 Alterar cores e tamanhos é divertido!</Text>
+      <Text style={[styles.card4, styles.pattern]}>✨ Desafio: tente recriar este layout!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,7 +22,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   titulo: {
-    fontSize: 33,
-    color: "orange"
+    fontSize: 30,
+    color: "orange",
+    marginBottom: 20
+  },
+  pattern: {
+    marginBottom: 20,
+    fontSize: 20,
+    padding: 5
+  },
+  card1: {
+    padding: 5,
+    backgroundColor: "#E6F2FF",
+    color: "#5F93BF",
+    borderRadius: 4 
+  },
+  card2: {
+    padding: 5,
+    backgroundColor: "#D4F5E1",
+    color: "#4E9F72",
+    borderRadius: 4
+  },
+  card3: {
+    padding: 5,
+    backgroundColor: "#FFF5E6",
+    color: "#FFB65F",
+    borderRadius: 4
+  },
+  card4: {
+    padding: 5,
+    backgroundColor: "#F3E6FF",
+    color: "#941595",
+    borderRadius: 4,
   }
 });
