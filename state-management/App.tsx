380bef7f8902c5899ac305291792ab46b8bc3dc0
Henrique A. R. <henrique4r.it@gmail.com>
2026-03-09 20:19:21 -0300

    feat(state-management): adiciona a imagem do chicolas na aplicação

--- state-management/App.tsx
+++ state-management/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import chicoHappy from './assets/chico-happy.jpg';
+import chicoSad from './assets/chico-sad.jpg';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={chicoSad} />
       <StatusBar style="auto" />
     </View>
   );
