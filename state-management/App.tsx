66b7f69bb242194da95bc352a36de708c69a662b
Henrique A. R. <henrique4r.it@gmail.com>
2026-03-09 21:53:49 -0300

    study(state-management): implementa a ação de toque na tela que chama a função de mudamça de estado

--- state-management/App.tsx
+++ state-management/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
 import chicoHappy from './assets/chico-happy.jpg';
 import chicoSad from './assets/chico-sad.jpg';
 import { useState } from 'react';
@@ -15,7 +15,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Image style={styles.tamanhoImagem} source={taFeliz ? chicoHappy : chicoSad} />
+      <TouchableOpacity onPress={handleHumor}>
+        <Image style={styles.tamanhoImagem} source={taFeliz ? chicoHappy : chicoSad} />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
