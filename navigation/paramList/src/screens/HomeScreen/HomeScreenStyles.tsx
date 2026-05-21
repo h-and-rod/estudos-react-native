c3832abc3998908bb80e278d6923aa2b28b6f827
Henrique A. R. <henrique4r.it@gmail.com>
2026-05-21 16:15:10 -0300

    style(Home): adiciona as configurações de estilo para a tela Home

---
+++ navigation/paramList/src/screens/HomeScreen/HomeScreenStyles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native"
+
+export const homeScreenStyles = StyleSheet.create({
+    textField: {
+        borderColor: 'rgb(51, 122, 210)',
+        borderWidth: 1,
+        paddingHorizontal: 12,
+        paddingVertical: 10,
+        borderRadius: 12,
+        margin: 10,
+        backgroundColor: '#FFF'
+    }
+})
